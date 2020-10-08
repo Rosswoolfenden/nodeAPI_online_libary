8ae66c2b22dd18319c2032c8d7f83793498c6289
Ross Woolfenden <rosswoolfenden@gmail.com>
2020-10-08 14:15:11 +0100

    clean up code;

--- database/mariaDbConnector.js
+++ database/mariaDbConnector.js
@@ -1,63 +1,25 @@
 const config = require('../config');
 const logger = require('../logging/WinstonLogging');
 const mariadb = require ('mariadb');
-// const mariaPool = require('../mariaDB/mariaPool');
 const log = logger.createLogger('SQL connector');
 
-// const pool1 = mariadb.createPool({
-//     host: config.mariadb.host,
-//     port: config.mariadb.port,
-//     user: config.mariadb.user,
-//     password: config.mariadb.password,
-//     database: config.mariadb.database,
-//     connectionLimit: 5,
-// });
-const pool1 = mariadb.createPool(config.mariadb);
+// gets maria db pool from config;
+const pool = mariadb.createPool(config.mariadb);
 
 
-const pool = mariadb.createPool({
-    host: 'localhost',
-    user: '304backend',
-    port: '3306',
-    password: 'qwerty',
-    database: '304libary',
-    connectionLimit: 5
-    });
-
-// // const pool2 = mariadb.createPool({
-// //     host: '172.17.0.1',
-// //     user: root,
-// //     da
-// // });
-
-async function getConnections() {
-    try {
-        console.log('the pool is is ' + pool1 );
-        return ;
-        // return pool1.getConnection();
-    } catch (e){
-        log.error('failed to get connection');
-        throw e;
-    }
-    
-}
-
-exports.query = async (q, params) => {
-    log.info('i have been called');
+// function to query mariadb database;
+exports.sqlquery = async (q, params) => {
     let connection;
     try{
-        log.info('we got this far');
-        connection = await getConnections();
-        log.info('connection is ' + connection);
-       // const res = await connection.query(q, params);
-       // log.info('res is ' + res.key);
-      //  return res;
+        connection = await pool.getConnection();
+        const res = await connection.query(q, params);
+        return res;
     } catch(e) {
         log.error({Error: e});
         throw e;
     } finally {
         if(connection) {
-            log.debug('ending connection')
+            log.debug('db connection end');
             connection.end();
         }
     }

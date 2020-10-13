ea1ab0df7c1d7c2ca36f0658184b863b6bdfe97f
Ross Woolfenden <rosswoolfenden@gmail.com>
2020-10-13 13:45:35 +0100

    added error handing to db connector

--- database/mariaDbConnector.js
+++ database/mariaDbConnector.js
@@ -2,6 +2,7 @@ const config = require('../config');
 const logger = require('../logging/WinstonLogging');
 const mariadb = require ('mariadb');
 const log = logger.createLogger('SQL connector');
+const { v4: uuidv4 } = require('uuid');
 
 // gets maria db pool from config;
 const pool = mariadb.createPool(config.mariadb);
@@ -15,12 +16,22 @@ exports.sqlquery = async (q, params) => {
         const res = await connection.query(q, params);
         return res;
     } catch(e) {
-        log.error({Error: e});
-        throw e;
+        const errorId = uuidv4();
+        log.error(Date.now(), errorId, )
+        throw new DatabaseExeption('Database Error', e.code, errorId);
     } finally {
         if(connection) {
-            log.debug('db connection end');
+            log.debug('mariadb connection has ended');
             connection.end();
         }
     }
 }
+
+
+function DatabaseExeption(msg, code, id) {
+    this.message = this.msg;
+    this.code = code;
+    this.id = id;
+    this.name = 'DatabaseException';
+}
+

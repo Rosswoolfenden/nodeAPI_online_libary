c31a7e3acd5f30c672d3920f081f2e3eaadffb78
woolfenr <rosswoolfenden@gmail.com>
2020-11-06 15:23:52 +0000

    added validation to book inputs

--- controllers/validation.js
+++ controllers/validation.js
@@ -1,17 +1,15 @@
 
 const {Validator, ValidationError, validate} = require('jsonschema');
-
-// const articleSchema = require('../schemas/article.json').definitions.article;
-// const categorySchema = require('../schemas/category.json').definitions.category;
-// const commentSchema = require('../schemas/comment.json').definitions.comment;
-
 const logger = require('../logging/WinstonLogging');
+
+const bookSchema = require('../schema/book.json').definitions.books;
 const user = require('../schema/users.json').definitions.user;
 const userUpdate = require('../schema/users.json').definitions.userUpdate;
 const log = logger.createLogger('Validation');
 
 const makeValidator = (schema, resource) => {
-
+  log.info(schema);
+  log.info(resource);
   const v = new Validator();
   const validationOptions = {
     throwError: true,
@@ -27,12 +25,12 @@ const makeValidator = (schema, resource) => {
       v.validate(body, schema, validationOptions);
       await next();
     } catch (error) {
+      log.info(error);
       if (error instanceof ValidationError) {
         log.error(error);
         ctx.body = {message: error.stack};
         ctx.status = 400;      
       } else { 
-        log.error(error);
         throw error;
       }
     }
@@ -41,4 +39,5 @@ const makeValidator = (schema, resource) => {
 }
 
 exports.validateUser = makeValidator(user, 'user');
-exports.validateUpdate = makeValidator(userUpdate, 'userUpdate');
+// exports.validateUpdate = makeValidator(userUpdate, 'userUpdate');
+exports.validateBook = makeValidator(bookSchema, 'book');

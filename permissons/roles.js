62895890587b91430652508bf033e31d636ee7b2
woolfenr <rosswoolfenden@gmail.com>
2020-11-09 19:32:56 +0000

    updated permissions

--- permissons/roles.js
+++ permissons/roles.js
@@ -2,21 +2,21 @@ const AccessControl = require("accesscontrol");
 const ac = new AccessControl();
 
 exports.roles = (function() {
-  ac.grant("basic")
-    .readOwn("profile")
-    .updateOwn("profile")
+  ac.grant('basic')
+    .readOwn('profile')
+    .updateOwn('profile')
     .deleteOwn('profile')
     .deleteOwn('books')
     .updateOwn('books')
     
 
-  ac.grant("admin")
-    .extend("basic")
-    .readAny("profile")
-    .updateAny("profile")
-    .deleteAny("profile")
+  ac.grant('admin')
+    .extend('basic')
+    .readAny('profile')
+    .updateAny('profile')
+    .deleteAny('profile')
     .updateAny('books')
-    .deleteAny('books');
+    .deleteAny('books')
 
   return ac;
 })();

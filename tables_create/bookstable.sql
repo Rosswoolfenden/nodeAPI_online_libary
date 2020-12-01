6072c6faf612e85e46d70d9e6bef0a916d120688
woolfenr <rosswoolfenden@gmail.com>
2020-12-01 18:25:39 +0000

    changed schema

--- tables_create/bookstable.sql
+++ tables_create/bookstable.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS books (
     ownerId INT NOT NULL,
     about TEXT,
     imageURL VARCHAR(2048),
+    author VARCHAR(32),
     status VARCHAR(32) DEFAULT "available",
     PRIMARY KEY(ID)
 );

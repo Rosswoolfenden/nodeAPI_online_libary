10797a35dddbda409b3863d5b0d2eb0b8979176b
woolfenr <rosswoolfenden@gmail.com>
2020-12-03 16:09:51 +0000

    removed uniqe key email

--- tables_create/usertable.sql
+++ tables_create/usertable.sql
@@ -2,7 +2,7 @@ CREATE TABLE IF NOT EXISTS users (
     ID INT NOT NULL AUTO_INCREMENT,
     firstName VARCHAR(32),  
     lastName VARCHAR(32),
-    username VARCHAR(16) UNIQUE NOT NULL,
+    username VARCHAR(16) NOT NULL,
     about TEXT,
     dateRegistered DATETIME DEFAULT CURRENT_TIMESTAMP,
     password VARCHAR(32),  

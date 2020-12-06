4795986ef0f5458b3c675c9b2ac99ab7af456010
woolfenr <rosswoolfenden@gmail.com>
2020-12-06 15:30:11 +0000

    new colum to add requester id

--- tables_create/messagesTable.sql
+++ tables_create/messagesTable.sql
@@ -1,9 +1,12 @@
 CREATE TABLE IF NOT EXISTS messages(
     ID INT NOT NULL AUTO_INCREMENT,
     ownerId INT NOT NULL,
+    requesterId INT NOT NULL,
     bookId INT NOT NULL,
     msg VARCHAR(1000), 
     PRIMARY KEY (ID),
     FOREIGN KEY (ownerId) REFERENCES users(ID),
+
+    FOREIGN KEY (requesterId) REFERENCES users(ID),
     FOREIGN KEY (bookId) REFERENCES books(ID)
 );
